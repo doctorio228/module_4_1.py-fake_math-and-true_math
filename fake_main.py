f5de36f1e841ee701b1c8f2011c9a44dc7444648
doctorio228 <kukhtin.a@list.ru>
2024-10-29 16:26:30 +0300

    Add files via upload

---
+++ fake_main.py
@@ -0,0 +1,5 @@
+def divid(first,second):
+    if second == 0:
+        return 'Ошибка'
+    else:
+        return first/second
---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_main import divid as div1
+from true_main import divid as div2
+result1 = div1(69, 3)
+result2 = div1(3, 0)
+result3 = div2(49, 7)
+result4 = div2(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
